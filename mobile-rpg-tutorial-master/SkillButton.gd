e5850147802b31f9f7b7b8a9a05db0cb95260549
Marco Veloso <marco.veloso@bcb.gov.br>
2022-04-29 16:28:55 -0300

    Skills parametrizadas via DICT

--- mobile-rpg-tutorial-master/SkillButton.gd
+++ mobile-rpg-tutorial-master/SkillButton.gd
@@ -2,4 +2,3 @@ extends "res://SkillsActions.gd"
 
 func _on_toggled(selected):	
 	execute_skill(text)
-	set_textbox("Triple Attack \nAttack 3 times in a row")
--- mobile-rpg-tutorial-master/SkillsActions.gd
+++ mobile-rpg-tutorial-master/SkillsActions.gd
@@ -3,20 +3,38 @@ extends Button
 const BattleUnits = preload("res://BattleUnits.tres")
 const Skills = preload("res://Skills.tscn")
 
+var skill_data = {
+	"SLASH": {
+		"damage":1,
+		"description":"Slash (Damage:1)  \nBasic slash attack"
+	},
+	"CROSS": {
+		"damage":2,
+		"description":"Slash (Damage:2)  \nCross slash attack"
+	},
+	"TRIPLE": {
+		"damage":3,
+		"description":"Slash (Damage:3)  \nTriple slash attack"
+	},
+	"COMBO": {
+		"damage":5,
+		"description":"Slash (Damage:5)  \nPowerful combo"
+	},
+	
+}
+
 func execute_skill(skill_name):
 	var enemy = BattleUnits.Enemy
 	var playerStats = BattleUnits.PlayerStats
+	var textbox = get_tree().current_scene.find_node("Textbox")		
 	
 	if enemy != null and playerStats != null:
 		var skill = Skills.instance().init(skill_name)
 		get_tree().current_scene.add_child(skill)
 		skill.global_position = enemy.global_position
 		
-		enemy.take_damage(2)
+		enemy.take_damage(skill_data[skill_name]["damage"])
 		playerStats.mp += 2
 		playerStats.ap -= 1
-	
-func set_textbox(description):
-	var textbox = get_tree().current_scene.find_node("Textbox")
-	
-	textbox.text = description
+		
+		textbox.text = skill_data[skill_name]["description"]
