e60cd0ffb6fc943bd5ebe8a6bdb6f9fa8aa7796d
Marco Veloso <marco.veloso@bcb.gov.br>
2022-04-29 11:00:03 -0300

    Funções de skill centralizadas

--- mobile-rpg-tutorial-master/ActionButton.gd
+++ mobile-rpg-tutorial-master/ActionButton.gd
@@ -1,6 +1,18 @@
 extends Button
 
 const BattleUnits = preload("res://BattleUnits.tres")
+const Attack = preload("res://Attacks.tscn")
 
-func _on_pressed():
-	pass
+func execute_skill(skill_name):
+	var enemy = BattleUnits.Enemy
+	var playerStats = BattleUnits.PlayerStats
+	
+	if enemy != null and playerStats != null:
+		var skill = Attack.instance().init(skill_name)
+		get_tree().current_scene.add_child(skill)
+		skill.global_position = enemy.global_position
+		
+		enemy.take_damage(2)
+		playerStats.mp += 2
+		playerStats.ap -= 1
+	
--- mobile-rpg-tutorial-master/SwordActionButton.gd
+++ mobile-rpg-tutorial-master/SwordActionButton.gd
@@ -1,18 +1,18 @@
 extends "res://ActionButton.gd"
 
-const Attack = preload("res://Attacks.tscn")
-
 func _on_pressed():
-	var enemy = BattleUnits.Enemy
-	var playerStats = BattleUnits.PlayerStats
+	execute_skill("triple")
 	
-	if enemy != null and playerStats != null:
-		var slash = Attack.instance().init("triple")
-		get_tree().current_scene.add_child(slash)
-		slash.global_position = enemy.global_position
-		
-		enemy.take_damage(2)
-		playerStats.mp += 2
-		playerStats.ap -= 1
+#	var enemy = BattleUnits.Enemy
+#	var playerStats = BattleUnits.PlayerStats
+#
+#	if enemy != null and playerStats != null:
+#		var slash = Attack.instance().init("triple")
+#		get_tree().current_scene.add_child(slash)
+#		slash.global_position = enemy.global_position
+#
+#		enemy.take_damage(2)
+#		playerStats.mp += 2
+#		playerStats.ap -= 1
 
 	
