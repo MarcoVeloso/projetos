53c9602c928d911603394576f2ba7d600b57e52f
Marco Veloso <marco.veloso@bcb.gov.br>
2022-05-27 11:42:51 -0300

    Explosion and Fireballs skills

--- AutoRPG/Objects/Player/PlayerData.gd
+++ AutoRPG/Objects/Player/PlayerData.gd
@@ -3,4 +3,4 @@ extends Node
 const max_hp = 10
 const max_ap = 100
 
-const selected_skills = ["SLASH", "CROSS", "CRESCENT", "ULTIMA", "", "", "", ""]
+const selected_skills = ["SLASH", "CROSS", "CRESCENT", "EXPLOSION", "FIREBALLS", "ULTIMA", "", ""]
--- AutoRPG/Objects/Skills/SkillsData.gd
+++ AutoRPG/Objects/Skills/SkillsData.gd
@@ -16,9 +16,19 @@ const data = {
 		"ap":5,
 		"description":"Crescent Slash - 5AP\nA powerful curved slash attack"
 	},
+	"EXPLOSION": {
+		"damage":5,
+		"ap":8,
+		"description":"Explosion - 8AP\nPowerful explosion"
+	},
+	"FIREBALLS": {
+		"damage":8,
+		"ap":10,
+		"description":"Fireballs - 10AP\nTwo fireballs"
+	},
 	"ULTIMA": {
 		"damage":10,
-		"ap":10,
-		"description":"Ultima Explosion - 10AP\nUltimate explosion magic! Hits very very hard!"
+		"ap":15,
+		"description":"Ultima Explosion - 15AP\nUltimate explosion magic! Hits very very hard!"
 	},
 }
