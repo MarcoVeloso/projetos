4715316855111ce72deb8af17c01da264a06f923
Marco Veloso <marco.veloso@bcb.gov.br>
2022-05-31 14:37:31 -0300

    New Aura to boss

--- AutoRPG/Objects/Player/PlayerData.gd
+++ AutoRPG/Objects/Player/PlayerData.gd
@@ -2,6 +2,6 @@ extends Node
 
 var max_hp = 10
 var max_ap = 100
-var start_ap = 1
+var start_ap = 100
 
 const selected_skills = ["SLASH", "DEFEND", "HEAL", "CROSS", "CRESCENT", "EXPLOSION", "FIREBALLS", "ULTIMA"]
