1332f35b1ffc6bf89ba9c564a03d2e3c0e3e23b4
Marco Veloso <marco.veloso@bcb.gov.br>
2022-06-02 17:18:10 -0300

    Adjusted enemy params

--- AutoRPG/GameScenes/StagesData.gd
+++ AutoRPG/GameScenes/StagesData.gd
@@ -8,11 +8,21 @@ const data = [
 	{
 		"name":"Tutorial",
 		"chest_base_gold":10,
-		"enemies":["SkeltonKNIFE", "SkeltonBOW", "SkeltonSPEAR", "SkeltonSWORD"],
+		"enemies":["SkeltonKNIFE", "SkeltonKNIFE", "SkeltonKNIFE"],
 	},
 	{
 		"name":"Dungeon 1",
 		"chest_base_gold":15,
-		"enemies":["SkeltonKNIFE", "SkeltonKNIFE", "SkeltonKNIFE"],
+		"enemies":["SkeltonKNIFE", "SkeltonKNIFE", "SkeltonBOW", "SkeltonKNIFE"],
+	},
+	{
+		"name":"Dungeon 2",
+		"chest_base_gold":15,
+		"enemies":["SkeltonKNIFE", "SkeltonBOW", "SkeltonSPEAR", "SkeltonBOW"],
+	},
+	{
+		"name":"Dungeon 2",
+		"chest_base_gold":15,
+		"enemies":["SkeltonBOW", "SkeltonBOW", "SkeltonSPEAR", "SkeltonSWORD", "SkeltonBOW"],
 	},
 ]
--- AutoRPG/Objects/Player/PlayerData.gd
+++ AutoRPG/Objects/Player/PlayerData.gd
@@ -2,7 +2,7 @@ extends Node
 
 var max_hp = 10
 var max_ap = 100
-var start_ap = 1
+var start_ap = 100
 var max_gold = 100
 var attack_power = 1
 var magic_power = 1
