327f21790178eb1da8d047892ceecd8909df0223
Marco Veloso <marco.veloso@bcb.gov.br>
2022-06-23 14:29:26 -0300

    Stages data update

--- AutoRPG/GameScenes/StagesData.gd
+++ AutoRPG/GameScenes/StagesData.gd
@@ -2,23 +2,33 @@ extends Node
 
 const data = {
 	"11":{
-		"name":"Tutorial",
+		"name":"Green Fields 1",
 		"chest_base_gold":10,
 		"enemies":["E_SkeltonKNIFE", "E_SkeltonKNIFE", "C_RED"],
 	},
 	"12":{
-		"name":"Dungeon 1",
+		"name":"Green Fields 2",
 		"chest_base_gold":15,
-		"enemies":["E_SkeltonKNIFE", "T_SPIKES", "E_SkeltonKNIFE", "C_WOOD", "E_SkeltonBOW", "E_SkeltonKNIFE", "C_RED"],
+		"enemies":["E_SkeltonKNIFE", "E_SkeltonKNIFE", "C_WOOD", "E_SkeltonBOW", "E_SkeltonKNIFE", "C_RED"],
 	},
 	"13":{
-		"name":"Dungeon 2",
+		"name":"Green Fields 3",
 		"chest_base_gold":20,
-		"enemies":["E_SkeltonKNIFE", "E_SkeltonBOW", "E_SkeltonSPEAR", "E_SkeltonBOW", "C_RED"],
+		"enemies":["E_SkeltonKNIFE", "T_SPIKES", "E_SkeltonBOW", "E_SkeltonSPEAR", "C_WOOD", "E_SkeltonBOW", "C_RED"],
 	},
 	"14":{
-		"name":"Dungeon 3",
+		"name":"Green Fields 4",
 		"chest_base_gold":25,
-		"enemies":["E_SkeltonBOW", "E_SkeltonBOW", "E_SkeltonSPEAR", "E_SkeltonSWORD", "E_SkeltonBOW", "C_RED"],
+		"enemies":["E_SkeltonBOW", "C_WOOD", "E_SkeltonBOW", "E_SkeltonSPEAR", "E_SkeltonSPEAR", "E_SkeltonSWORD", "T_SPIKES", "E_SkeltonBOW", "C_RED"],
+	},
+	"15":{
+		"name":"Green Fields 5",
+		"chest_base_gold":30,
+		"enemies":["E_SkeltonBOW", "E_SkeltonSPEAR", "E_SkeltonSPEAR", "C_WOOD", "E_SkeltonBOW", "T_SPIKES", "E_SkeltonSPEAR", "E_SkeltonSWORD", "E_SkeltonSWORD", "E_SkeltonSPEAR", "C_RED"],
+	},
+	"16":{
+		"name":"Green Fields 6",
+		"chest_base_gold":35,
+		"enemies":["E_SkeltonKNIFE", "E_SkeltonBOW", "E_SkeltonSPEAR", "T_SPIKES", "E_SkeltonSWORD", "E_SkeltonSPEAR", "C_WOOD", "E_SkeltonSWORD", "E_SkeltonSWORD", "E_SkeltonSWORD", "C_RED"],
 	},
 }
