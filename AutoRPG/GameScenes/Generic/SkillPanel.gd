f59d5969996dcf7b29c7ff692db1a0a4b5907e75
Marco Veloso <marco.veloso@bcb.gov.br>
2022-06-28 14:53:50 -0300

    Adjustments on stats upgrades

--- AutoRPG/GameScenes/Generic/SkillPanel.gd
+++ AutoRPG/GameScenes/Generic/SkillPanel.gd
@@ -3,6 +3,7 @@ extends Control
 onready var title = $Panel/Title
 onready var req = $Panel/Req
 onready var desc = $Panel/Description
+onready var buttonText = $Panel/Button/Text
 
 var stats = {
 	"HP":"Increase Health Points",
@@ -13,9 +14,10 @@ var stats = {
 }
 
 func _ready():
-	drawStats(self.name)
+	drawStats(self.name, 20, 9758)
 
 
-func drawStats(stat):
-	title.bbcode_text = GameData.icon[stat] + "+"
+func drawStats(stat, increment, cost):
+	title.bbcode_text = GameData.icon[stat] + "+" + str(increment)
 	desc.bbcode_text = stats[stat]
+	buttonText.text = "$\n" + str(cost)
