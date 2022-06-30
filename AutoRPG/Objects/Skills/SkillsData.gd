ba116b715ceb25081f2fbc0782f161e5caa441d3
Marco Veloso <marco.veloso@bcb.gov.br>
2022-06-30 17:13:05 -0300

    Attributes in SkillsData

--- AutoRPG/Objects/Skills/SkillsData.gd
+++ AutoRPG/Objects/Skills/SkillsData.gd
@@ -94,10 +94,44 @@ var passives = {
 	},
 }
 
+var attributes = {
+	"HP": {
+		"title":"HP↑",
+		"desc":"Increase Health Points",
+		"values":[10,20,30,40,50],
+		"costs":[0,50,200,500,1200],
+	},
+	"AP": {
+		"title":"AP↑",
+		"desc":"Increase max Action Points",
+		"values":[5,10,20,30,40],
+		"costs":[0,50,200,500,1200],
+	},
+	"ATK": {
+		"title":"ATK↑",
+		"desc":"Increase Attack Power",
+		"values":[1,2,3,4,5],
+		"costs":[0,50,200,500,1200],
+	},
+	"MAG": {
+		"title":"MAG↑",
+		"desc":"Increase Magic Power",
+		"values":[1,2,3,4,5],
+		"costs":[0,50,200,500,1200],
+	},
+	"WALLET": {
+		"title":"WALLET↑",
+		"desc":"Increase max $ carried",
+		"values":[100, 200, 500, 1000, 2000, 5000, 10000],
+		"costs":[0, 100, 200, 500, 1000, 2000, 5000],
+	},
+}
+
 
 func _ready():
 	skillsDescription()
 	replaceStats(passives)
+	replaceStats(attributes)
 
 
 func skillsDescription():
