83c059beca64245f2053962e36f42b3dd2be14a9
Marco Veloso <marco.veloso@bcb.gov.br>
2022-07-01 15:48:09 -0300

    Fixed SkillPanel spacing onShop

--- AutoRPG/GameScenes/Shop/Shop.gd
+++ AutoRPG/GameScenes/Shop/Shop.gd
@@ -5,9 +5,11 @@ onready var skill_type = $UI/SkillsBackPanel/Title
 onready var buttonLeft = $UI/ButtonLeft
 onready var buttonRight = $UI/ButtonRight
 
+
 func _ready():
 	loadSkills()
-	
+
+
 func loadSkills():
 	var type = skill_type.text.to_lower()
 	var skills = SkillsData.shop[type]
@@ -21,8 +23,6 @@ func loadSkills():
 			skill.drawSkill(skills[index])
 		else:
 			skill.visible = false
-	
-	skills_panel.add_constant_override("separation", clamp((6 - max_index) * 2, 0, 7))
 
 
 func _on_ButtonLeft_pressed():
@@ -31,8 +31,8 @@ func _on_ButtonLeft_pressed():
 
 func _on_ButtonRight_pressed():
 	buttonTypes(buttonRight.text)
-	
-	
+
+
 func buttonTypes(type_text):
 	skill_type.text = type_text
 	loadSkills()
@@ -47,4 +47,4 @@ func buttonTypes(type_text):
 		"Passives":
 			buttonLeft.text = "Skills"
 			buttonRight.text = "Attributes"
-	
+
