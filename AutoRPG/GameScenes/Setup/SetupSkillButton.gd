cd0960f4a0495a975f1ccbad425fac79047170d6
Marco Veloso <marco.veloso@bcb.gov.br>
2022-08-12 16:35:24 -0300

    Fixed popup overlap on skill description

--- AutoRPG/GameScenes/Setup/SetupSkillButton.gd
+++ AutoRPG/GameScenes/Setup/SetupSkillButton.gd
@@ -2,9 +2,11 @@ extends MenuButton
 
 onready var popup = self.get_popup()
 onready var setup = get_node("/root/Setup")
+var popup_y_position
 
  
 func _ready():
+	popup.grow_vertical = Control.GROW_DIRECTION_BEGIN
 	popup.connect("index_pressed", self, "_on_index_pressed")
 	popup.connect("visibility_changed", self, "_on_visibility_changed")
 
@@ -20,23 +22,26 @@ func _on_index_pressed(index):
 	if "ActivesPanel" in str(get_path()):
 		PlayerData.setPlayerSkill(skill, self.name)
 		setup.updateSkillsScreen()
-		yield(get_tree().create_timer(0.01), "timeout")
-		self.emit_signal("pressed")
 	else:
 		PlayerData.setPlayerPassive(skill)
 		setup.updatePassiveDescription(skill)
 
 
 func _on_visibility_changed():
-	if popup.visible:
-		popup.rect_position.y = self.rect_global_position.y
-
-		if popup.rect_position.x > 30:
-			popup.rect_position.x = 4
+	if self.text:
+		setup.updateSkillDescription(self.text)
+		
+		if popup.visible:
+			if popup_y_position:
+				popup.rect_position.y = popup_y_position
+			else:
+				popup.rect_position.y -= 14
+				popup_y_position = popup.rect_position.y
 		else:
-			popup.rect_position.x = 46
-
-
-func _on_pressed():
-	self.pressed = true
-	setup.updateSkillDescription(self.text)
+			yield(get_tree().create_timer(0.01), "timeout")
+			self.pressed = true
+			
+	#		if popup.rect_position.x > 30:
+	#			popup.rect_position.x = 4
+	#		else:
+	#			popup.rect_position.x = 46
